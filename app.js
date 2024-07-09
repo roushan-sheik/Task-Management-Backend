608a67171f43cb5290e8df3b8432610fbe9f7d77
roushan-sheik <roushansheik@gmail.com>
2024-07-09 22:23:51 +0600

    bug fixed

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 //middleware
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://task-management-faf4c.web.app/"],
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
+app.use(cors({}));
 app.use(express.json());
 app.use(cookieParser());
 
